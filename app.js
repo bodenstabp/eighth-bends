f2d58aea941cf386d1450aa0514ae4722fc307a8
Paul Bodenstab <bodenstabp@gmail.com>
2022-09-06 17:25:48 -0400

    style changes

--- app.js
+++ app.js
@@ -1,48 +1,37 @@
 const button = document.querySelector('button');
 const displayedNumber = document.querySelector('.result');
 
-// round answer to nearest eighth
-
 button.addEventListener('click', e => {
   e.preventDefault();
-
   const distance = document.querySelector('#measurement').value;
-  const product = +distance * 1.414;
+  const product = multiplier(distance, 1.414);
   displayedNumber.innerHTML = roundNumber(product);
 });
 
+function multiplier(distance, multiplier) {
+  const dist = +distance;
+  return dist * multiplier;
+}
+
 function roundNumber(number) {
   const decimal = number - Math.floor(number);
+  console.log(decimal);
   const newDecimal = () => {
     if (decimal >= 0.9375) {
       return 1;
-    } else if (decimal >= 0.875) {
+    } else if (decimal >= 0.875 || decimal >= 0.8125) {
       return 0.875;
-    } else if (decimal >= 0.8125) {
-      return 0.875;
-    } else if (decimal >= 0.75) {
+    } else if (decimal >= 0.75 || decimal >= 0.6875) {
       return 0.75;
-    } else if (decimal >= 0.6875) {
-      return 0.75;
-    } else if (decimal >= 0.625) {
+    } else if (decimal >= 0.625 || decimal >= 0.5625) {
       return 0.625;
-    } else if (decimal >= 0.5625) {
-      return 0.625;
-    } else if (decimal >= 0.5) {
+    } else if (decimal >= 0.5 || decimal >= 0.4375) {
       return 0.5;
-    } else if (decimal >= 0.4375) {
-      return 0.5;
-    } else if (decimal >= 0.375) {
+    } else if (decimal >= 0.375 || decimal >= 0.3125) {
       return 0.375;
-    } else if (decimal >= 0.3125) {
-      return 0.375;
-    } else if (decimal >= 0.25) {
+    } else if (decimal >= 0.25 || decimal >= 0.1875) {
       return 0.25;
-    } else if (decimal >= 0.1875) {
-      return 0.25;
-    } else if (decimal >= 0.125) {
-      return 0.125;
-    } else if (decimal >= 0.0625) {
+    } else if (decimal >= 0.125 || decimal >= 0.0625) {
       return 0.125;
     }
     return 0;
